9e73172b1aa5d631b48e422e85934c993b6c3da3
walker <hiniwoo@163.com>
2018-12-06 21:24:06 +0800

    bianyi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,35 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+	<div>
+	</div>
   </div>
 </template>
 
-<script>
+<script>s
+import vueWaterfallEasy from 'vue-waterfall-easy'
 export default {
   name: 'HelloWorld',
+  components:{
+	  vueWaterfallEasy
+  },
+  data() {
+  	return {
+  		imgsArr: []
+  	}
+  },
+  mounted() {
+  	this.imgsArr=['https://image.fenlaishop.com/product/admin_2c908381653c873d01657fe13ba40090_1541991781760.jpg?x-oss-process=image/resize,m_fill,h_320,w_320,limit_0',
+					'https://image.fenlaishop.com/product/admin_2c908381653c873d01657fe13ba40090_1541991781760.jpg?x-oss-process=image/resize,m_fill,h_320,w_320,limit_0',
+					'https://image.fenlaishop.com/product/admin_2c908381653c873d01657fe13ba40090_1541991781760.jpg?x-oss-process=image/resize,m_fill,h_320,w_320,limit_0']
+  },
   props: {
     msg: String
-  }
+  },
+	methods: {
+		getstring() {
+			
+		}
+	},
 }
 </script>
 
